ee0dfd9a55f69330bd9b222ea53fe480b45bd28f
Vikram Ramavarapu <vikramr2@illinois.edu>
2023-04-17 17:48:04 -0500

    styling

--- helpers/profile_children.sh
+++ helpers/profile_children.sh
@@ -1,8 +1,10 @@
 #!/bin/bash 
 pid=$1
 timestamp=$2
+
 readarray -t children < <(pgrep -P $pid)
 sampling_time=$(date +%s)
+
 if [ "${#children[*]}" -gt 0 ]; then
     python3 helpers/get_pid_data.py $sampling_time ${children[*]} >> profile_$timestamp.csv
 fi
