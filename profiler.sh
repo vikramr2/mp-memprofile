a01a591d2bef681ffe4cb88972dd7f3c6649f9cc
Vikram Ramavarapu <vikramr2@illinois.edu>
2023-04-17 16:53:23 -0500

    stored forked pids in array

--- profiler.sh
+++ profiler.sh
@@ -5,7 +5,12 @@ pid=$(pgrep -P $$)
 trap "kill $pid 2> /dev/null" EXIT
 
 while kill -0 $pid 2> /dev/null; do
-    pgrep -P $pid
+    readarray -t children < <(pgrep -P $pid)
+    if [ "${#children[*]}" -gt 0 ]; then
+        for child in ${children[*]}; do
+            echo $child
+        done
+    fi
     echo "---"
     sleep 1
 done
