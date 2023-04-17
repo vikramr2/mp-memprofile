5984ab67bd43d9f9d702d1070683e8c8dd962769
Vikram Ramavarapu <vikramr2@illinois.edu>
2023-04-17 16:18:25 -0500

    created test case

---
+++ tests/test1.py
@@ -0,0 +1,18 @@
+import multiprocessing as mp
+from os import getpid
+from time import sleep
+
+def task():
+    print(f'Hello, I\'m {getpid()} !!')
+    sleep(5)
+    print(f'{getpid()} says bye !!')
+
+def main():
+    procs = [mp.Process(target=task) for _ in range(10)]
+    for p in procs:
+        p.start()
+    for p in procs:
+        p.join()
+
+if __name__ == '__main__':
+    main()
