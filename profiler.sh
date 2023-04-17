a23c8018c38fe5e8a67b2409159f9a7e38b1b999
Vikram Ramavarapu <vikramr2@illinois.edu>
2023-04-17 17:45:36 -0500

    improve sampling frequency

--- profiler.sh
+++ profiler.sh
@@ -8,10 +8,6 @@ pid=$(pgrep -P $$)
 trap "kill $pid 2> /dev/null" EXIT
 
 while kill -0 $pid 2> /dev/null; do
-    readarray -t children < <(pgrep -P $pid)
-    sampling_time=$(date +%s)
-    if [ "${#children[*]}" -gt 0 ]; then
-        python3 helpers/get_pid_data.py $sampling_time ${children[*]} >> profile_$timestamp.csv
-    fi
+    ./helpers/profile_children.sh $pid $timestamp &
     sleep 1
 done
