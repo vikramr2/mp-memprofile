b1a7447f0127035ed1e94495d21f561c699ab699
Vikram Ramavarapu <vikramr2@illinois.edu>
2023-04-17 18:10:01 -0500

    stored sample rate in var

--- profiler.sh
+++ profiler.sh
@@ -1,4 +1,6 @@
 #!/bin/bash 
+sample_rate=1 # modify this to sample at different rates
+
 timestamp=$(date +%s)
 echo "pid,time,rss,pss,uss,shared,shared_file" > profile_$timestamp.csv
 
@@ -10,5 +12,5 @@ trap "kill $pid 2> /dev/null" EXIT
 while kill -0 $pid 2> /dev/null; do
     ./helpers/profile_parent.sh $pid $timestamp &
     ./helpers/profile_children.sh $pid $timestamp &
-    sleep 1
+    sleep $sample_rate
 done
